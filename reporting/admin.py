ae102c1b14419d76e426f5b02bdc757f34d27fc3
fllfl <rimu@fisherieslogistics.com>
2017-08-18 14:16:35 +1200

    register the rest urls

--- reporting/admin.py
+++ reporting/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from reporting.models import FishCatch, Species, Trip, Vessel, Port,\
+    NonFishProtectedSpeciesInteractionEvent, FishingEvent, FishReciever, ProcessedState
 
+
+admin.site.register(FishCatch)
+admin.site.register(Species)
+admin.site.register(Trip)
+admin.site.register(Vessel)
+admin.site.register(Port)
+admin.site.register(NonFishProtectedSpeciesInteractionEvent)
+admin.site.register(FishingEvent)
+admin.site.register(FishReciever)
+admin.site.register(ProcessedState)
 # Register your models here.
