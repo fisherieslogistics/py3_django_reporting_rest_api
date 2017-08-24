f75841233cccb51c061b786f3d0146a84e0a836a
fllfl <rimu@fisherieslogistics.com>
2017-08-24 20:58:15 +1200

    import models to views

--- reporting/views.py
+++ reporting/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from models import FishCatch, FishingEvent, Species
 # Create your views here.
 @csrf_exempt
 def fishingEventWithCatches(request):
