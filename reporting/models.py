4c21f7136783aa4c90a98aca07ac884146543bee
fllfl <rimu@fisherieslogistics.com>
2017-08-25 00:49:53 +1200

    make org have a uuid default

--- reporting/models.py
+++ reporting/models.py
@@ -4,7 +4,7 @@ from django.contrib.postgres.fields import JSONField
 from django.contrib.auth.models import AbstractUser
 
 class Organisation(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     fullName = models.CharField(max_length=120)
 
 
