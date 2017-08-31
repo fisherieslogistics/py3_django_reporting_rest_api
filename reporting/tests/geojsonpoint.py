1494cd675328db2c33548baccc33f95eab10dc12
fllfl <rimu@fisherieslogistics.com>
2017-09-01 10:20:40 +1200

    add some useful generators

---
+++ reporting/tests/geojsonpoint.py
@@ -0,0 +1,38 @@
+import random
+
+
+def maybe(percent=50):
+    return random.randrange(100) < percent
+
+
+def geojsonpoint(config):
+    lat = config['lat']
+    lon = config['lon']
+    p = '{"type": "Point",'
+
+    def thegenerator():
+        latmove = random.randrange(10) / 1000
+        lonmove = random.randrange(10) / 1000
+        newlat = lat
+        newLon = lon
+        if(maybe()):
+            newlat -= latmove
+        else:
+            newlat += latmove
+        if(maybe()):
+            newLon -= lonmove
+        else:
+            newLon += lonmove
+        c = '"coordinates":[{l}, {la}]'.format(l=lon, la=lat)
+        gp = p + c + '}'
+        while(True):
+            yield gp
+
+    return thegenerator
+
+
+def geopoint(config):
+    return geojsonpoint(config)()
+
+
+GENERATORS = {'geojsonpoint': geopoint}
