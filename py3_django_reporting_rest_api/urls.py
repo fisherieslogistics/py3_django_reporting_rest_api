b7d66061ff59a3c35eec51bb8c45b447b1a93e90
martin-fll <30398335+martin-fll@users.noreply.github.com>
2017-08-31 18:25:19 +1200

    move rest api to subpath /rest-api/

--- py3_django_reporting_rest_api/urls.py
+++ py3_django_reporting_rest_api/urls.py
@@ -39,7 +39,7 @@ router.register(r"organisations", OrganisationViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r"^", include(router.urls)),
+    url(r"^rest-api/", include(router.urls)),
     url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     url(r"^admin/", admin.site.urls),
     url(r'^api-token-auth/', obtain_jwt_token),
