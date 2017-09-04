0b39aa218adc3b5812b3d6795c6c13cdd0c02604
fllfl <rimu@fisherieslogistics.com>
2017-09-05 11:52:06 +1200

    merge it up
    megres

--- py3_django_reporting_rest_api/settings_common.py
+++ py3_django_reporting_rest_api/settings_common.py
@@ -19,9 +19,7 @@ AUTH_USER_MODEL = 'reporting.User'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 ALLOWED_HOSTS = ['*']
-
 # Application definition
 
 INSTALLED_APPS = [
