0315c38007b3215966c351ca1d30d8cc930fe34e
fllfl <rimu@fisherieslogistics.com>
2017-09-06 10:10:09 +1200

    delete past tests crap

--- reporting/tests/prepare_test_database.py
+++ reporting/tests/prepare_test_database.py
@@ -1,10 +1,15 @@
 from reporting.models import User
-from reporting.models import Organisation
+from reporting.models import Organisation, Trip, Port, Vessel
 
 User.objects.filter(username='restadmin').delete();\
 User.objects.create_superuser('restadmin', 'restadmin@test.com', 'restinpeace')
 
-Organisation.objects.filter(fullName="Rimu Industries REST").delete()
+orgs = Organisation.objects.filter(fullName="Rimu Industries REST")
+for o in orgs:
+    Trip.objects.filter(organisation=o).delete()
+    Port.objects.filter(organisation=o).delete()
+    Vessel.objects.filter(organisation=o).delete()
+orgs.delete()
 org = Organisation.objects.create(fullName="Rimu Industries REST")
 User.objects.filter(username='resttester').delete()
 User.objects.create_user('resttester', 'rest@test.com', 'testrester', organisation=org)
