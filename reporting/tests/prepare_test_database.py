c9115112a9aa21f6b85ed81be2954c53c569bbec
fllfl <rimu@fisherieslogistics.com>
2017-09-06 09:54:42 +1200

    delete the old org before create the new

--- reporting/tests/prepare_test_database.py
+++ reporting/tests/prepare_test_database.py
@@ -4,6 +4,7 @@ from reporting.models import Organisation
 User.objects.filter(username='restadmin').delete();\
 User.objects.create_superuser('restadmin', 'rest@test.com', 'restinpeace')
 
+Organisation.objects.filter(fullName="Rimu Industries REST").delete()
 org = Organisation.objects.create(fullName="Rimu Industries REST")
-User.objects.filter(username='resttester').delete();\
+User.objects.filter(username='resttester').delete()
 User.objects.create_user('resttester', 'rest@test.com', 'testrester', organisation=org)
